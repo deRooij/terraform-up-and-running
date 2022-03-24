0da6b28535b4ab7c1d6a6cecdc4ec157f1593c85
Rik <rikrooij.de@gmail.com>
2022-03-24 12:35:58 +0100

    Multiple servers + extended readme

---
+++ 2_multiple_servers/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-07d8796a2b0f8d29c"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+    description = "The from and to port for our webserver"
+    type = number
+    default = 8080
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
+}
+
+output "public_dns" {
+    value = aws_instance.example.public_dns
+    description = "The public DNS of the instance"
+}
+
+output "instance_arn" {
+  value = aws_instance.example.arn
+  description = "The arn of the instance"
+}
--- Readme.md
+++ Readme.md
@@ -1,4 +1,26 @@
-# Access your aws account using console
+# Getting started
+
+## Access your aws account using console
+
+If you don't already have an AWS account go to aws.amazon.com to sign up.
+This will create you as root user but for security we want to use IAM users with specific rights.
+
+### Create IAM user
+
+Go to the IAM console and create a new user, generate a new access key and save it to your local machine.
+To run all examples add the following Policies to this IAM user:
+
+- AmazonEC2FullAccess
+- AmazonS3FullAccess
+- AmazonDynamoDBFullAccess
+- AmzonRDSFullAccess
+- CloudWatchFullAccess
+- IAMFullAccess
+
+### Use IAM user
+
+Make sure to have terraform installed from: terraform.io
+To use the created credentials we set them as environment variables by running the following commands:
 
 ```
 export AWS_ACCESS_KEY_ID=(your access key id)
